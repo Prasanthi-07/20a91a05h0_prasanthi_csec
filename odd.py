6d13e8d18cae035891da941844385f554257088e
Prasanthi-07 <84372562+Prasanthi-07@users.noreply.github.com>
2021-05-24 12:45:32 +0530

    Create odd.py

---
+++ odd.py
@@ -0,0 +1,16 @@
+a=input("enter value")
+b=input("enter value")
+if('a%2==1'):
+    print("odd")
+else:
+        print("Even")
+if('b%2==1'):
+    print("odd")
+else:
+        print("even")
+output----
+enter value7
+enter value6
+odd
+odd
+>>> 
