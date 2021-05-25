733b763248ed71b616c9f63c55076800561df652
Prasanthi-07 <84372562+Prasanthi-07@users.noreply.github.com>
2021-05-25 14:23:07 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,12 @@
+a=input("enter value")
+if('a%2==0'):
+    print("a is divisible by 2")
+elif('a%2==0'):
+    print("a is divisible by 3")
+elif('a%2==0'):
+    print("a is divisible by 5")
+else:
+    print("a is not divisible by given numbers")
+    output---
+enter value 6
+a is divisible by 2    
